2771a051c96694d2b44f701a465e44a4cf4dd258
oliveeah <olliebennettx@gmail.com>
2022-09-07 16:52:41 +0100

    ordered and shows results

--- Timer Leaderboards Database/MainWindow.xaml.cs
+++ Timer Leaderboards Database/MainWindow.xaml.cs
@@ -24,10 +24,12 @@ namespace Timer_Leaderboards_Database
     {
 
         Stopwatch stopwatch = new();
-
+        double duration; 
+        bool button_clicked = false;
         
-        SqlConnection con = new SqlConnection(@"DataSource=OLLIEPC\SQLEXPRESS; Initial Catalog=record;");
+        SqlConnection con = new SqlConnection(@"Data Source=OLLIEPC\SQLEXPRESS;Initial Catalog=Timer;Integrated Security=True;");
         SqlCommand com;
+        SqlDataReader rd;
 
         public MainWindow()
         {
@@ -40,14 +42,64 @@ namespace Timer_Leaderboards_Database
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             stopwatch.Stop();
-            double duration = stopwatch.Elapsed.TotalSeconds;
-            
+            duration = stopwatch.Elapsed.TotalSeconds;
+            duration = Math.Round(duration, 2);
+            button_clicked = true;
+
         }
 
         private void SubmitScore_Click(object sender, RoutedEventArgs e)
         {
-            con.Open();
-            com
+            while (button_clicked = true)
+            {
+                con.Open();
+                com = new SqlCommand("insert into TimerLeaderboards values('" + Enter_Name.Text.ToUpper() + "','" + duration + "')", con);
+                com.ExecuteNonQuery();
+                MessageBox.Show("Data saved in database");
+                con.Close();
+            }
+          
         }
+
+        private void Retrieve_Scores_Click(object sender, RoutedEventArgs e)//retrieves particular player info
+        {
+            /*  con.Open();
+              com = new SqlCommand("select PlayerID, UName, Score from TimerLeaderboards order by Score DESC;", con);
+
+              com.Parameters.AddWithValue("PlayerID", usernames.Text);
+              SqlDataReader reader;
+              reader = com.ExecuteReader();
+              if(reader.Read())
+              {
+                  usernames.Text = reader["UName"].ToString();
+                  scores.Text = reader["Score"].ToString();
+              }
+              else
+              {
+                  MessageBox.Show("No Data Found");
+              }
+
+              //com.ExecuteNonQuery();
+              con.Close();*/
+
+            string sql = ("select * from TimerLeaderboards order by Score DESC");
+            
+                con.Open();
+                MessageBox.Show("Connection Active");
+                com = new SqlCommand(sql, con);
+                rd = com.ExecuteReader();
+
+            while (rd.Read())
+            {
+                MessageBox.Show(rd.GetValue(1) + "-" + rd.GetValue(2));//numbers refer to columns
+            }
+            con.Close();
+
+
+
+        }
+
+
+
     }
 }
