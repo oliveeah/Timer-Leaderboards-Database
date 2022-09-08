9b0fe3ca0e392b9260ae2466f11de9b9b67d8094
oliveeah <olliebennettx@gmail.com>
2022-09-08 22:41:40 +0100

    test

--- Timer Leaderboards Database/MainWindow.xaml.cs
+++ Timer Leaderboards Database/MainWindow.xaml.cs
@@ -1,32 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
 using System.Data.SqlClient;
-using System.ComponentModel;
-using System.Drawing;
+using System.Diagnostics;
+using System.Windows;
 
 namespace Timer_Leaderboards_Database
 {
- 
+
     public partial class MainWindow : Window
     {
 
         Stopwatch stopwatch = new();
-        double duration; 
-        bool button_clicked = false;
-        
+        double duration;
+
+
         SqlConnection con = new SqlConnection(@"Data Source=OLLIEPC\SQLEXPRESS;Initial Catalog=Timer;Integrated Security=True;");
         SqlCommand com;
         SqlDataReader rd;
@@ -34,31 +20,32 @@ namespace Timer_Leaderboards_Database
         public MainWindow()
         {
             InitializeComponent();
-            
+
             stopwatch.Start();
         }
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+
             stopwatch.Stop();
             duration = stopwatch.Elapsed.TotalSeconds;
             duration = Math.Round(duration, 2);
-            button_clicked = true;
+
 
         }
 
         private void SubmitScore_Click(object sender, RoutedEventArgs e)
         {
-            while (button_clicked = true)
-            {
-                con.Open();
-                com = new SqlCommand("insert into TimerLeaderboards values('" + Enter_Name.Text.ToUpper() + "','" + duration + "')", con);
-                com.ExecuteNonQuery();
-                MessageBox.Show("Data saved in database");
-                con.Close();
-            }
-          
+
+            con.Open();
+            com = new SqlCommand("insert into TimerLeaderboards values('" + Enter_Name.Text.ToUpper() + "','" + duration + "')", con);
+            com.ExecuteNonQuery();
+            MessageBox.Show("Data saved in database");
+            con.Close();
+
+
         }
 
         private void Retrieve_Scores_Click(object sender, RoutedEventArgs e)//retrieves particular player info
@@ -82,12 +69,11 @@ namespace Timer_Leaderboards_Database
               //com.ExecuteNonQuery();
               con.Close();*/
 
-            string sql = ("select * from TimerLeaderboards order by Score DESC");
-            
-                con.Open();
-                MessageBox.Show("Connection Active");
-                com = new SqlCommand(sql, con);
-                rd = com.ExecuteReader();
+            string sql = ("select * from TimerLeaderboards order by Score DESC");//select from the timerleaderboards table and order by score (high to low)
+
+            con.Open();
+            com = new SqlCommand(sql, con);
+            rd = com.ExecuteReader();
 
             while (rd.Read())
             {
